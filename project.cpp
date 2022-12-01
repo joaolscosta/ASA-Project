9e2c05b72159f99e78f58004558961ee2ba9f719
João Costa <joaoluissaraivacosta@gmail.com>
2022-12-01 16:14:27 +0000

    started to implement asa project

--- project.cpp
+++ project.cpp
@@ -1,113 +1,79 @@
 #include <vector>
 #include <iostream>
 
-using std::endl, std::string;
-
 typedef std::vector<std::vector<int>> matrix;
 
 int main()
 {
-  int people_in_network, relation_count;
+  int lines = 0, columns = 0;
+  int max_square_dim = 0;
+  // int total_combinations = 0;
 
-  std::cin >> people_in_network;
-  std::cin.ignore();
-  std::cin >> relation_count;
+  std::cin >> lines;
+  std::cin >> columns;
+
+  int c_values[lines];
+
+  for (int i = 0; i < lines; i++) // input c values of each line
+  {
+    std::cin >> c_values[i];
+  }
+
+  // percorrer c_values para ver a maior dimensão possivel
+
+  for (int i = columns; i > 0; i--)
+  {
+    int cont = 0;
+    for (int j = i; j > 0; j--)
+    {
+      if (c_values[j - 1] >= i)
+      {
+        cont++;
+        if (cont == i)
+        {
+          max_square_dim = i;
+        }
+      }
+      else
+      {
+        break;
+      }
+    }
+    if (max_square_dim != 0)
+    {
+      break;
+    }
+  }
 
   matrix m = {};
 
-  // initialize vector
-  for (int i = 0; i < people_in_network; ++i)
+  for (int i = 0; i < lines; i++) // initialize matrix
   {
-    std::vector<int> vec = {};
-    for (int j = 0; j < people_in_network; j++)
+    std::vector<int> vector = {};
+    for (int j = 0; j < columns; j++)
     {
-      vec.push_back(0);
+      vector.push_back(0);
     }
-    m.push_back(vec);
+    m.push_back(vector);
   }
 
-  // populate relations
-  for (int i = 0; i < relation_count; ++i)
+  for (int i = 0; i < lines; i++) // fill colored squares
   {
-    int origin, target;
-    std::cin >> origin >> target;
-
-    m.at(origin - 1).at(target - 1)++;
-  }
-
-  // each row is the people with n friends
-  std::vector<int> hist1(people_in_network, 0);
-  // each row is how many people have n people as friends
-  std::vector<int> hist2(people_in_network, 0);
-  for (int i = 0; i < people_in_network; ++i)
-  {
-    int friend_count = 0;
-    int inverse_friend_count = 0;
-    for (int j = 0; j < people_in_network; ++j)
+    for (int j = 0; j < c_values[i]; j++)
     {
-      if (m.at(i).at(j) > 0)
-      {
-        ++friend_count;
-      }
-      if (m.at(j).at(i) > 0)
-      {
-        ++inverse_friend_count;
-      }
-    }
-    hist1.at(friend_count)++;
-    hist2.at(inverse_friend_count)++;
-  }
-
-  std::cout << "Histograma 1" << std::endl;
-  for (size_t i = 0; i < hist1.size(); ++i)
-  {
-    std::cout << hist1.at(i) << std::endl;
-  }
-
-  std::cout << "Histograma 2" << std::endl;
-  for (size_t i = 0; i < hist1.size(); ++i)
-  {
-    std::cout << hist2.at(i) << std::endl;
-  }
-
-  matrix common_friends;
-  // initialize vector
-  for (int i = 0; i < people_in_network; ++i)
-  {
-    std::vector<int> vec = {};
-    for (int j = 0; j < people_in_network; j++)
-    {
-      vec.push_back(0);
-    }
-    common_friends.push_back(vec);
-  }
-
-  for (int i = 0; i < people_in_network; ++i)
-  {
-    for (int j = 0; j < people_in_network; ++j)
-    {
-      int common_count = 0;
-      for (int k = 0; k < people_in_network; ++k)
-      {
-        if (m.at(i).at(k) > 0 && m.at(j).at(k) > 0)
-        {
-          ++common_count;
-        }
-      }
-      common_friends.at(i).at(j) = common_count;
+      m.at(i).at(j) = 1;
     }
   }
 
-  std::cout << "Common Friends" << std::endl;
-
-  for (int i = 0; i < people_in_network; ++i)
+  for (int i = 0; i < lines; i++) // print matrix
   {
-    for (int j = 0; j < people_in_network; j++)
+    for (int j = 0; j < c_values[i]; j++)
     {
-      std::cout << common_friends.at(i).at(j);
+      std::cout << m.at(i).at(j) << "|";
     }
-    std::cout << std::endl;
+    std::cout << "\n";
   }
+  std::cout << max_square_dim << std::endl;
 
   return 0;
 }
