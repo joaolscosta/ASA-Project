a0b3a7c73d014088ff16bf808ada5f958060eb8f
Daniel Nunes <danielnunes70@gmail.com>
2022-12-27 16:33:49 +0000

    feito(acho)

--- proj2/p.cpp
+++ proj2/p.cpp
@@ -4,63 +4,58 @@
 using namespace std;
 
 struct edge {
-    int vertice1;
-    int vertice2;
-    int w;
+    int vertice_1;
+    int vertice_2;
+    int weight;
 };
 
-
-bool comp(edge a, edge b) {
-    if(a.w > b.w) {
+bool comp(edge edge_1, edge edge_2) {
+    if(edge_1.weight > edge_2.weight) {
         return true;
     }
-
     return false;
 }
 
-int find(int vertice, int par[]) {
-    if(par[vertice] == -1) {
+int search_free(int vertice, int parent[]) {
+    if(parent[vertice] == -1) {
         return vertice;
     }
-    return par[vertice] = find(par[vertice], par);
+    return parent[vertice] = search_free(parent[vertice], parent);
 }
 
-
-
-int main()
-{
+int main() {
 
     int vertices = 0, edges = 0;
+    int vertice_1 = 0, vertice_2 = 0;
+    int max_trade = 0;
 
     cin >> vertices;
     cin >> edges;
 
-    edge ar[edges];
-    int par[vertices +1];
+    edge edges_array[edges];
+    int parent[vertices + 1];
 
     for(int i = 1; i <= vertices; i++) {
-        par[i] = -1;
+        parent[i] = -1;
     }
 
-    int vertice1 = 0, vertice2 = 0;
     for (int i = 0; i < edges; i++) { 
-
-        cin >> ar[i].vertice1 >> ar[i].vertice2 >> ar[i].w;
+        cin >> edges_array[i].vertice_1 >> edges_array[i].vertice_2 >> edges_array[i].weight;
     }
 
-    int sum = 0;
-    sort(ar, ar + edges, comp);
+    sort(edges_array, edges_array + edges, comp);
 
     for (int i = 0; i < edges; i++) { 
 
-        vertice1 = find(ar[i].vertice1, par);
-        vertice2 = find(ar[i].vertice2, par);
+        vertice_1 = search_free(edges_array[i].vertice_1, parent);
+        vertice_2 = search_free(edges_array[i].vertice_2, parent);
 
-        if(vertice1 != vertice2) {
-            sum += ar[i].w;
-            par[vertice1] = vertice2;
+        if(vertice_1 != vertice_2) {
+            max_trade += edges_array[i].weight;
+            parent[vertice_1] = vertice_2;
         }
     }
-    cout << sum << endl;
+
+    cout << max_trade << endl;
     return 0;
 }
